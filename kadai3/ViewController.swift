0bff43e1511cab700b7c2bba545524b44aec4d38
akio0911 <akio0911@gmail.com>
2024-03-18 11:54:00 +0900

    feedback

--- kadai3/ViewController.swift
+++ kadai3/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         value1Label.text = ""
         value2Label.text = ""
     }
-    
+
     // アウトレット定義
     @IBOutlet weak var value1: UITextField!
     @IBOutlet weak var value2: UITextField!
@@ -29,20 +29,15 @@ class ViewController: UIViewController {
         // value1の処理
         if button1.isOn {
             value1 = -value1
-            value1Label.text = String(value1)
-        }else if !button1.isOn {
-            value1Label.text = String(value1)
         }
-        
+        value1Label.text = String(value1)
+
         // value2の処理
         if button2.isOn {
             value2 = -value2
-            value2Label.text = String(value2)
-        }else if !button2.isOn {
-            value2Label.text = String(value2)
         }
-        
+        value2Label.text = String(value2)
+
         calculateResult.text = String(value1 + value2)
     }
 }
-
